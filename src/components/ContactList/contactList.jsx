c0ef725834fc8c88866da96be79c8ec011ca1475
Oleg Troian <sleite4r@gmail.com>
2024-07-14 22:56:18 +0200

    finish css

--- src/components/ContactList/contactList.jsx
+++ src/components/ContactList/contactList.jsx
@@ -1,11 +1,17 @@
-import Contact from './Contact/ContactList';
-import styles from './ContactList.module.css';
+import Contact from '../Contact/Contact';
+import styles from "./ContactList.module.css";
 
 const ContactList = ({ contacts, onDeleteContact }) => {
   return (
     <ul className={styles.list}>
       {contacts.map(({ id, name, number }) => (
-        <Contact key={id} id={id} name={name} number={number} onDelete={onDeleteContact} />
+        <Contact
+          key={id}
+          id={id}
+          name={name}
+          number={number}
+          onDelete={onDeleteContact}
+        />
       ))}
     </ul>
   );
--- src/components/SearchBox/searchBox.jsx
+++ src/components/SearchBox/searchBox.jsx
@@ -3,8 +3,8 @@ import styles from './SearchBox.module.css';
 const SearchBox = ({ value, onChange }) => {
   return (
     <div className={styles.SearchBox}>
-      <label htmlFor="search">Find contacts by name</label>
-      <input
+      <label className={ styles.LabelBox} htmlFor="search">Find contacts by name</label>
+      <input className={ styles.BoxInput}
         type="text"
         name='search'
         value={value}
